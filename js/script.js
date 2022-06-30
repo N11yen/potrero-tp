95a0fcb23615aedd7a849d6d74b92a2a499f71c8
Matias Niyen Dargel <102557595+N11yen@users.noreply.github.com>
2022-06-30 17:48:57 -0300

    Add files via upload
    "comits de js clase 9"

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+
+function cambiarfondo() { 
+
+    document.body.style.backgroundColor= "black";
+    document.getElementById("header1").style.backgroundColor = "black";
+    document.getElementById("header1").style.color = "#90ffe9";
+    document.getElementById("nav1").style.backgroundColor = "white";
+    document.getElementById("nav1").style.color = "red";
+    document.getElementById("section1").style.backgroundColor = "black";
+    document.getElementById("section1").style.color = "white";
+    document.getElementById("footer1").style.backgroundColor = "black";
+    console.log (document.getElementById("nav1").style.color)
+    }
